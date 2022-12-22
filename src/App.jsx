7dee8556e691a99ecbad1640c92b99977d9d5b09
guus <ghligtvoet@gmail.com>
2022-12-22 20:20:20 +0100

    basic game logic

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,59 @@
 import { useState } from 'react'
 import './App.css'
 
-// components
-import Slots from './components/Slots'
-
 function App() {
-  const [count, setCount] = useState(0);
+  const [reels, setReels] = useState([]);
+  const [gameOver, setGameOver] = useState(false);
+  const [gameResult, setGameResult] = useState("");
   const reel1 = ["🍑","🍆","🍒","🛎️","🍫","🍥"];
+  const reel2 = ["🍑","🍆","🍒","🛎️","🍫","🍥","🍥","🍥","🍥","🍥","🍥"];
+  // const reel3 = ["🍑","🍆","🍒","🛎️","🍫","🍥"];
+
+  const clearReels = () => {
+    setReels([]);
+    setGameOver(false);
+  }
+
+  const checkIfWon = () => {
+    if (reels.every(symbol => symbol === reels[0])) {
+      setGameResult("won")
+    } else {
+      setGameResult("lost")
+    }
+  }
+
+  const pickSymbol = (reel) => {
+      let randomSymbol = reel[Math.floor(Math.random() * reel.length)]
+      setReels(reels => [...reels, randomSymbol])
+      
+      if (reels.length+1 === 3) {
+        setGameOver(true);
+        checkIfWon();
+      }
+  }
 
   return (
-    <Slots reel1={reel1}/>
+    <div className='container'>
+      { !gameOver && (
+        <div>
+          <h1>Fruitmeister 3000</h1>
+          <h1>{reels}</h1>
+          <button
+            onClick={() => pickSymbol(reel2)}
+          >
+            Click me!
+          </button>
+        </div> )
+      }
+      {
+        gameOver && (
+        <div>
+          <h1>Game Over! You {gameResult}!</h1>
+          <h1>{reels}</h1>
+          <button onClick={() => clearReels()}>Play Again</button>
+        </div> )
+      }
+    </div>
   )
 }
 
--- src/components/Slots.jsx
+++
@@ -1,7 +0,0 @@
-const Slots = ({reel1}) => {
-  return (
-    <div>Slots</div>
-  )
-}
-
-export default Slots
